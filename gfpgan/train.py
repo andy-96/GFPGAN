fc5fffe2957e2c28e87f5764835f50d8321762ec
Andy Chen <47676621+andy-96@users.noreply.github.com>
2021-12-29 18:36:08 +0100

    Update train.py

--- gfpgan/train.py
+++ gfpgan/train.py
@@ -2,9 +2,7 @@
 import os.path as osp
 from basicsr.train import train_pipeline
 
-import gfpgan.archs
-import gfpgan.data
-import gfpgan.models
+from .. import archs, data, models
 
 if __name__ == '__main__':
     root_path = osp.abspath(osp.join(__file__, osp.pardir, osp.pardir))
